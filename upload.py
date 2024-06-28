b8de35146e880430cffed6e51bd18b31335e4a18
AndresMirelesEbury <andres.mireles@ebury.com>
2024-06-28 16:03:10 +0200

    Push with error in local ccy in setp 2

--- upload.py
+++ upload.py
@@ -9,18 +9,31 @@ def get_schemas(table):
     return schemas[table]
 
 
-def upload_table(df, t_name, bq_client, config):
+def upload_table(df, t_name, bq_client, config, date_str):
     schema = get_schemas(t_name)
 
     file_format = "NEWLINE_DELIMITED_JSON"
     local_tmp_file = f"{t_name}.json"
     df.to_json(local_tmp_file, orient="records", lines=True)
 
-    write_method = "WRITE_TRUNCATE"
+    write_method = 'WRITE_APPEND'
 
     dst_table = config["outputs"]["dataset"] + "." + t_name
 
+    date = pd.to_datetime(date_str)
+    date_str = date.strftime('%Y-%m-%d')
+    df['balance_date'] = date_str
     
+    # upload
+    print(f'Uploading to `{config["project"]["project_id"] + "." + dst_table}`...')
+    file_format = "NEWLINE_DELIMITED_JSON"
+    local_tmp_file = "tmp_upload_file.json"
+    df.to_json(local_tmp_file, orient="records", lines=True)
+
+    # Remove previos executions if exists
+    delete_query = f"delete from {dst_table} where date(balance_date) = date('{date_str}')"
+    bq_client.query(delete_query, result_to_df=False)
+
     bq_client.upload_local_file(
         source=local_tmp_file,
         destination_table=dst_table,
@@ -29,6 +42,5 @@ def upload_table(df, t_name, bq_client, config):
         write_disposition=write_method,
     )
 
-    print("Upload to BQ done!",dst_table)
-
     os.remove(local_tmp_file)
+    print(f'Upload successful')
